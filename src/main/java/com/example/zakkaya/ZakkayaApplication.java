389f22c548d0f38a9cb54455b08244a4ba9fb814
desktop <kuroko.keiko@gmail.com>
2025-07-06 17:25:38 +0900

    最初のコミット

---
+++ src/main/java/com/example/zakkaya/ZakkayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zakkaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakkayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakkayaApplication.class, args);
+	}
+
+}

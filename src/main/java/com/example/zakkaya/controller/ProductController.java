94dcb1e046d4c1dd0413640dd1a1de3871337995
desktop <kuroko.keiko@gmail.com>
2025-07-06 21:29:07 +0900

    ProductController.java追加

---
+++ src/main/java/com/example/zakkaya/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.example.zakkaya.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.zakkaya.service.ProductService;
+import com.example.zakkaya.entity.*;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.findAllProducts();
+        return ResponseEntity.ok(products);
+    }
+}
